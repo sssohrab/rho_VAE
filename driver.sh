05e175633fb57cf6576c9b55bce4c8b1c6163ab3
Maurits Diephuis <mdiephuis@gmail.com>
2019-09-12 20:48:02 +0200

    code update :

--- driver.sh
+++ driver.sh
@@ -1,23 +1,23 @@
-#
-python main.py --uid 'VanillaVAE' --epochs 15 --dataset-name 'mnist'
-python main.py --uid 'INFO_VAE' --epochs 15 --dataset-name 'mnist'
-python main.py --uid 'CNN_VAE' --epochs 15 --dataset-name 'mnist'
-python main.py --uid 'BETAVAE' --epochs 15 --dataset-name 'mnist'
+# #
+# python main.py --uid 'VanillaVAE' --epochs 15 --dataset-name 'mnist'
+# python main.py --uid 'INFO_VAE' --epochs 15 --dataset-name 'mnist'
+# python main.py --uid 'CNN_VAE' --epochs 15 --dataset-name 'mnist'
+# python main.py --uid 'BETAVAE' --epochs 15 --dataset-name 'mnist'
 
 
-python main.py --uid 'RHO_VanillaVAE' --epochs 15 --dataset-name 'mnist' --rho
-python main.py --uid 'RHO_INFO_VAE' --epochs 15 --dataset-name 'mnist' --rho
-python main.py --uid 'RHO_CNN_VAE' --epochs 15 --dataset-name 'mnist' --rho
-python main.py --uid 'RHO_BETAVAE' --epochs 15 --dataset-name 'mnist' --rho
+# python main.py --uid 'RHO_VanillaVAE' --epochs 15 --dataset-name 'mnist' --rho
+# python main.py --uid 'RHO_INFO_VAE' --epochs 15 --dataset-name 'mnist' --rho
+# python main.py --uid 'RHO_CNN_VAE' --epochs 15 --dataset-name 'mnist' --rho
+# python main.py --uid 'RHO_BETAVAE' --epochs 15 --dataset-name 'mnist' --rho
 
 
-python main.py --uid 'VanillaVAE' --epochs 15 --dataset-name 'fashion'
-python main.py --uid 'INFO_VAE' --epochs 15 --dataset-name 'fashion'
+# python main.py --uid 'VanillaVAE' --epochs 15 --dataset-name 'fashion'
+# python main.py --uid 'INFO_VAE' --epochs 15 --dataset-name 'fashion'
 python main.py --uid 'CNN_VAE' --epochs 15 --dataset-name 'fashion'
-python main.py --uid 'BETAVAE' --epochs 15 --dataset-name 'fashion'
+# python main.py --uid 'BETAVAE' --epochs 15 --dataset-name 'fashion'
 
 
-python main.py --uid 'RHO_VanillaVAE' --epochs 15 --dataset-name 'fashion' --rho
-python main.py --uid 'RHO_INFO_VAE' --epochs 15 --dataset-name 'fashion' --rho
+# python main.py --uid 'RHO_VanillaVAE' --epochs 15 --dataset-name 'fashion' --rho
+# python main.py --uid 'RHO_INFO_VAE' --epochs 15 --dataset-name 'fashion' --rho
 python main.py --uid 'RHO_CNN_VAE' --epochs 15 --dataset-name 'fashion' --rho
-python main.py --uid 'RHO_BETAVAE' --epochs 15 --dataset-name 'fashion' --rho
+# python main.py --uid 'RHO_BETAVAE' --epochs 15 --dataset-name 'fashion' --rho
